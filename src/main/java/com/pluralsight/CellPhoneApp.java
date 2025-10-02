decdb5b71fdc7c9c136d94d415f810cf77befe3f
keisha De La Rosa <keishdlr@gmail.com>
2025-10-02 15:55:29 -0400

    ask user questions and added variables to hold value

--- src/main/java/com/pluralsight/CellPhoneApp.java
+++ src/main/java/com/pluralsight/CellPhoneApp.java
@@ -6,10 +6,30 @@ public class CellPhoneApp {
 public Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        CellPhone serialNumber = new CellPhone(918);
-        CellPhone model = new CellPhone("Samsung S23 Ultra");
-        CellPhone carrier = new CellPhone("T-Mobile");
-        CellPhone owner = new CellPhone("Keisha");
+        System.out.println("What is the serial number?");
+        int serialNumber = myScanner.nextInt();
+
+        System.out.println("What model is the phone?");
+        String model = myScanner.nextLine();
+
+        System.out.println("Who is the carrier?");
+        String carrier = myScanner.nextLine();
+
+        System.out.println("What is the phone number?");
+        int phoneNumber = myScanner.nextInt();
+
+        System.out.println("Who is the owner of the phone?");
+        String owner = myScanner.nextLine();
+
+
+
+
+
+        // CellPhone serialNumber = new CellPhone(918);
+       // CellPhone model = new CellPhone("Samsung S23 Ultra");
+       // CellPhone carrier = new CellPhone("T-Mobile");
+       // CellPhone phoneNumber = new CellPhone(7180102233);
+       // CellPhone owner = new CellPhone("Keisha");
     }
 
 }
